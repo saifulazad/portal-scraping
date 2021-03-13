31f1b8f1d33a1dd341951735a380f08ee1ede305
azad <mr.saiful.azad@gmail.com>
2021-03-13 13:42:19 +0600

    Edit read me

--- extractor/bulk_job_processor.py
+++ extractor/bulk_job_processor.py
@@ -1,29 +1,29 @@
 import time
-
 import requests
 from mapper.maper import Mapper
+import logging
 
+logger = logging.getLogger(__name__)
 
 def fetch_page(url):
     r = requests.get(url)
     return r.text
 
 
-def get_all_job_details(contents):
+def get_all_job_details(job_links):
     url_list = []
     jobs_details = []
-    for x in contents:
-        url = f'https://jobs.bdjobs.com/{x.decode("utf-8")}'
+    for job_link in job_links:
+        url = 'https://jobs.bdjobs.com/{}'.format(job_link)
         url_list.append(url)
     for url in url_list:
-
         try:
             body = {}
             page = fetch_page(url)
-            # print(url)
+            logger.error(url)
             ob = Mapper(page=page)
             body = ob._read_from_HTML()
-            print(body)
+            # print(body)
 
         except Exception as ex:
 
@@ -37,3 +37,13 @@ def get_all_job_details(contents):
             data = {**body, **keys}
             jobs_details.append(data)
     return jobs_details
+
+
+def read_jobs_links(filename):
+    """
+
+    :param filename:
+    :return:
+    """
+    file = open(filename, 'r')
+    return [line.strip() for line in file.readlines()]
