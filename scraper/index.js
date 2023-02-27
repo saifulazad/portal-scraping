4db15e916c2a61ddfd0b4a2c37a4c6e57b154b46
saifulazad <mr.saiful.azad@gmail.com>
2023-02-27 19:16:52 +0600

    apply std linting

--- scraper/index.js
+++ scraper/index.js
@@ -1,17 +1,16 @@
-const chromium = require('chrome-aws-lambda');
-const extractor = require("./extractor");
-const s3uploader = require("./s3_uploader");
+const chromium = require('chrome-aws-lambda')
+const extractor = require('./extractor')
+const s3uploader = require('./s3_uploader')
 exports.handler = async (event) => {
-
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
     headless: true,
-    ignoreHTTPSErrors: true,
-  });
+    ignoreHTTPSErrors: true
+  })
   JobLinks = await extractor.scraper(browser)
-  await browser.close();
-  s3uploader.upload(JobLinks);
-};
+  await browser.close()
+  s3uploader.upload(JobLinks)
+}
--- scraper/s3_uploader.js
+++ scraper/s3_uploader.js
@@ -1,18 +1,18 @@
-const AWS = require('aws-sdk');
-exports.upload = async (content)=>{
-    const dateString = new Date().toISOString().split('-');
-    const year = dateString[0];
-    const month = dateString[1];
-    const day = dateString[2].split('T')[0];
-    let params = {
-        Bucket: "extractor-service-dev/joblinksfile",
-        Key: (`${year}-${month}-${day}`),
-        Body: content.join("\r\n")
-    };
-    try {
-        let uploadPromise = await new AWS.S3().putObject(params).promise();
-        console.log("Successfully uploaded data to bucket");
-    } catch (e) {
-        console.log("Error uploading data: ", e);
-    }
+const AWS = require('aws-sdk')
+exports.upload = async (content) => {
+  const dateString = new Date().toISOString().split('-')
+  const year = dateString[0]
+  const month = dateString[1]
+  const day = dateString[2].split('T')[0]
+  const params = {
+    Bucket: 'extractor-service-dev/joblinksfile',
+    Key: (`${year}-${month}-${day}`),
+    Body: content.join('\r\n')
+  }
+  try {
+    const uploadPromise = await new AWS.S3().putObject(params).promise()
+    console.log('Successfully uploaded data to bucket')
+  } catch (e) {
+    console.log('Error uploading data: ', e)
+  }
 }
