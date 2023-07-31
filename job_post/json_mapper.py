af5adeb0e64179ad50fa5d6dba41307ffa1efbae
Muazzem Hossain <muazzem.mamun@gmail.com>
2023-07-31 10:41:04 +0600

    remove class and add output json file path

--- job_post/json_mapper.py
+++ job_post/json_mapper.py
@@ -1,71 +1,61 @@
 import argparse
 import json
 
-
-class JSONMapper:
+def load_json_data(json_file):
     """
-        A class to handle loading and filtering data from a JSON file.
+    Load JSON data from the input file.
 
-        Attributes:
-            json_file (str): The path to the input JSON file.
-            data (dict): The loaded JSON data.
+    Parameters:
+        json_file (str): The path to the input JSON file.
 
-        Methods:
-            load_json_data(): Load JSON data from the input file and store it in the 'data' attribute.
-            filter_json_data(): Filter the loaded JSON data and return a dictionary with selected fields.
-
-        Example:
-            # Create a JSONMapper instance and process the JSON data
-            mapper = JSONMapper("input.json")
-            mapper.load_json_data()
-            result = mapper.filter_json_data()
+    Returns:
+        dict: The loaded JSON data.
     """
+    with open(json_file) as f:
+        return json.load(f)
 
-    def __init__(self, json_file):
-        self.json_file = json_file
-        self.data = None
+def filter_json_data(data):
+    """
+    Filter the loaded JSON data and return a dictionary with selected fields.
 
-    def load_json_data(self):
-        with open(self.json_file) as f:
-            self.data = json.load(f)
+    Parameters:
+        data (dict): The loaded JSON data.
 
-    def filter_json_data(self):
-        """
-            Filter the loaded JSON data and return a dictionary with selected fields.
+    Returns:
+        dict: A dictionary containing selected fields from the JSON data.
 
-            Returns:
-                dict: A dictionary containing selected fields from the JSON data.
+    Raises:
+        ValueError: If the JSON data is not loaded.
+    """
+    if not data:
+        raise ValueError("JSON data not loaded")
 
-            Raises:
-                ValueError: If the JSON data is not loaded.
-        """
-
-        if not self.data:
-            raise ValueError("JSON data not loaded")
-
-        result = {}
-        for field in self.data["fields"]:
-            if field["type"] == "DROPDOWN":
-                field_label = field["label"]
-                value_id = field["value"][0]
-                options = field["options"]
-                option_text = next((option["text"] for option in options if option["id"] == value_id), None)
-                if option_text is not None:
-                    result.setdefault(field_label, []).append(option_text)
-            else:
-                label = field["label"]
-                value = field["value"]
-                result[label] = value
-        return result
-
+    result = {}
+    for field in data["fields"]:
+        if field["type"] == "DROPDOWN":
+            field_label = field["label"]
+            value_id = field["value"][0]
+            options = field["options"]
+            option_text = next((option["text"] for option in options if option["id"] == value_id), None)
+            if option_text is not None:
+                result.setdefault(field_label, []).append(option_text)
+        else:
+            label = field["label"]
+            value = field["value"]
+            result[label] = value
+    return result
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="JSON Mapper")
-    parser.add_argument("json_file", help="Path to the input JSON file")
+    parser.add_argument("json_file", help="Path to the input JSON file", type=argparse.FileType('r'))
 
     args = parser.parse_args()
 
-    mapper = JSONMapper(args.json_file)
-    mapper.load_json_data()
-    merged_result = mapper.filter_json_data()
-    print(json.dumps(merged_result, indent=2))
+    data = load_json_data(args.json_file.name)
+    json_data = filter_json_data(data)
+
+    # Get the output file path by adding "_output" before the file extension
+    output_file_path = args.json_file.name.replace(".json", "_output.json")
+
+    with open(output_file_path, 'w') as output_file:
+        json.dump(json_data, output_file, indent=2)
