1cc33765fef1dec82bcd143d83f55a7547a55c0f
Saiful Azad <mr.saiful.azad@gmail.com>
2024-03-04 19:49:18 +0600

    Add terrform

--- infrastructure/importer/main.tf
+++ infrastructure/importer/main.tf
@@ -5,103 +5,71 @@ terraform {
     region = "ap-southeast-1"
   }
 }
+provider "aws" {
+  region = "ap-southeast-1"
+}
+#IAM Permission for lamda
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role-2"
 
-
-resource "aws_iam_role_policy" "revoke_keys_role_policy" {
-  name = var.lambda_iam_policy_name
-  role = aws_iam_role.lambda_iam.id
-
-  policy = <<EOF
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-        "s3:*",
-        "logs:*"
-      ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
 }
+
+#Create Lambda
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/CloudWatchFullAccess",]
+}
+
 data "archive_file" "archive_zip_validate" {
   type        = "zip"
-  source_dir  = "../job_post"
-  output_path = "jobpost.zip"
+  source_dir  = "../../job_post"
+  output_path = "job_post.zip"
 }
 
-# Creating Lambda resource
-resource "aws_lambda_function" "extractor_lambda_handler" {
-  function_name    = var.function_name
-  role             = aws_iam_role.lambda_iam.arn
-  handler          = "${var.handler_name}.lambda_handler"
-  description      = ""
-  runtime          = var.runtime
-  timeout          = var.timeout
-  architectures    = ["arm64"]
-  layers           = [var.lambda_layers]
-  filename         = data.archive_file.archive_zip_validate.output_path
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "job_post.zip"
+  function_name = "job_importer"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
   source_code_hash = data.archive_file.archive_zip_validate.output_base64sha256
-}
 
-# Creating s3 resource for invoking to lambda function
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-}
+  runtime = "python3.9"
 
-# Adding S3 bucket as trigger to my lambda and giving the permissions
-resource "aws_s3_bucket_notification" "aws_lambda_trigger" {
-  bucket = aws_s3_bucket.bucket.id
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.extractor_lambda_handler.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "joblinksfile/"
+  environment {
+    variables = {
+      foo = "bar"
+    }
   }
-  depends_on = [aws_lambda_permission.lambda_permission]
-}
-data "archive_file" "archive_scraper" {
-  type        = "zip"
-  source_dir  = "../scraper"
-  output_path = "scraper.zip"
-}
-
-# Creating Lambda resource
-resource "aws_lambda_function" "scraper_lambda_handler" {
-  function_name    = "scraper-lambda-handler"
-  role             = aws_iam_role.lambda_iam.arn
-  description      = "https://github.com/saifulazad/portal-scraping"
-  handler          = "index.handler"
-  runtime          = "nodejs12.x"
-  timeout          = var.timeout
-  memory_size      = 1024
-  architectures    = ["x86_64"]
-  layers           = ["arn:aws:lambda:ap-southeast-1:940443069190:layer:puppeteer-chrome:2"]
-  filename         = data.archive_file.archive_scraper.output_path
-  source_code_hash = data.archive_file.archive_scraper.output_base64sha256
-}
-
-
-
-// Allow CloudWatch to invoke our function
-resource "aws_lambda_permission" "allow_cloudwatch_to_invoke" {
-  function_name = aws_lambda_function.scraper_lambda_handler.function_name
-  statement_id  = "CloudWatchInvoke"
-  action        = "lambda:InvokeFunction"
-  source_arn = aws_cloudwatch_event_rule.scraper_lambda_event.arn
-  principal  = "events.amazonaws.com"
-}
-
-// Create the "cron" schedule
-resource "aws_cloudwatch_event_rule" "scraper_lambda_event" {
-  name                = "bd-jobs-cron"
-  description = "trigger lambda function"
-  schedule_expression = var.schedule
-}
-
-resource "aws_cloudwatch_event_target" "invoke_scraper_lambda_handler" {
-  rule = aws_cloudwatch_event_rule.scraper_lambda_event.name
-  arn  = aws_lambda_function.scraper_lambda_handler.arn
 }
