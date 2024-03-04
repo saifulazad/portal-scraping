8261566016638a5f2625abadaffc0b8201b02f37
Saiful Azad <mr.saiful.azad@gmail.com>
2024-03-04 20:13:50 +0600

    reformate code

--- job_post/lambda_function.py
+++ job_post/lambda_function.py
@@ -7,9 +7,10 @@ import googlemaps
 
 # address
 
+
 def find_location(address):
-        # API key is passed as an environment variable in the Lambda function
-    api_key = os.environ['GOOGLE_MAP_API_KEY']
+    # API key is passed as an environment variable in the Lambda function
+    api_key = os.environ["GOOGLE_MAP_API_KEY"]
     gmaps = googlemaps.Client(key=api_key)
 
     # Geocoding an address
@@ -17,12 +18,15 @@ def find_location(address):
 
     # Return the result as JSON
     return geocode_result
+
+
 def get_address(fields):
     for field in fields:
-        if field['label'] == 'Location':
-            return field['value'] or ''
-    return ''
-    
+        if field["label"] == "Location":
+            return field["value"] or ""
+    return ""
+
+
 def upload_jobs_details(key, content, bucket_name):
     """
 
@@ -33,29 +37,30 @@ def upload_jobs_details(key, content, bucket_name):
     """
     s3 = boto3.resource("s3")
     key = "jobs-post/" + key
-    address_as_text = get_address(content['fields'])
+    address_as_text = get_address(content["fields"])
     if address_as_text:
-        content['address'] = find_location(address_as_text)
-    else: 
-        content['address'] =[]
-        
+        content["address"] = find_location(address_as_text)
+    else:
+        content["address"] = []
+
     data = json.dumps(content).encode("utf-8")
-    
+
     s3_response = s3.Object(bucket_name, key).put(Body=data)
     print(s3_response)
 
 
 def lambda_handler(event, context):
-    
+
     prefix = datetime.datetime.now().strftime("%y/%m/%d")
-    payload = (json.loads(event['body'])['data'])
-    key = payload['submissionId']
+    payload = json.loads(event["body"])["data"]
+    key = payload["submissionId"]
     file_name = f"{prefix}/{key}.json"
-    # print(event) 
-    upload_jobs_details(key= file_name, content=payload, bucket_name='extractor-service-dev')
+    # print(event)
+    upload_jobs_details(
+        key=file_name, content=payload, bucket_name="extractor-service-dev"
+    )
 
 
 if __name__ == "__main__":
     data = {"Records": [{"s3": {"object": {"key": "joblinksfile/2023-02-23"}}}]}
     lambda_handler(data, {})
-
