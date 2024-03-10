c9f9a0a6b0cfc1fbde10fb47c1bdc712cbb4f3de
Saiful Azad <mr.saiful.azad@gmail.com>
2024-03-10 22:45:30 +0600

    change function name

--- infrastructure/jobpost/main.tf
+++ infrastructure/jobpost/main.tf
@@ -20,29 +20,29 @@ data "aws_iam_policy_document" "assume_role" {
 
 # Creating Lambda IAM resource
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "lambda_importer_job_post"
+  name               = "lambda_role_job_post"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "arn:aws:iam::aws:policy/CloudWatchFullAccess",
   ]
 }
-data "archive_file" "archive_job_importer_zip_validate" {
+data "archive_file" "archive_validate" {
   type        = "zip"
-  source_dir  = "../../job_importer"
-  output_path = "job_importer.zip"
+  source_dir  = "../../job_post"
+  output_path = "job_post.zip"
 }
 
 # Lambda resource for import job
-resource "aws_lambda_function" "job_importer_lambda_handler" {
-  filename         = "job_importer.zip"
-  function_name    = "job-importer"
+resource "aws_lambda_function" "lambda_handler" {
+  filename         = "job_post.zip"
+  function_name    = "job-post-save"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   description      = "https://github.com/saifulazad/portal-scraping/tree/master/job_post"
   architectures    = ["arm64"]
   layers           = [var.lambda_layer]
-  source_code_hash = data.archive_file.archive_job_importer_zip_validate.output_base64sha256
+  source_code_hash = data.archive_file.archive_validate.output_base64sha256
   runtime          = "python3.9"
   timeout          = var.timeout
   environment {
@@ -53,7 +53,7 @@ resource "aws_lambda_function" "job_importer_lambda_handler" {
 }
 
 resource "aws_lambda_function_url" "lambda_function_url" {
-  function_name      = aws_lambda_function.job_importer_lambda_handler.function_name
+  function_name      = aws_lambda_function.lambda_handler.function_name
   authorization_type = "NONE"
   cors {
     allow_origins = ["*"]
