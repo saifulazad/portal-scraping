f9fe94f66ac227f53dd79c7f8ac56a89a69a11fa
Saiful Azad <mr.saiful.azad@gmail.com>
2024-03-10 22:27:28 +0600

    Add output to function_url

--- infrastructure/jobpost/variables.tf
+++ infrastructure/jobpost/variables.tf
@@ -4,3 +4,6 @@ variable "google_map_api_key" {
 variable "lambda_layer" {
   default = "arn:aws:lambda:ap-southeast-1:940443069190:layer:google-map:2"
 }
+variable "timeout" {
+  default = "10" # the unit is in second
+}
