310d167ff26a6bdcb877817ba86023967a968c5f
Saiful Azad <mr.saiful.azad@gmail.com>
2024-03-10 23:22:48 +0600

    Add doc

--- infrastructure/importer/variables.tf
+++ infrastructure/importer/variables.tf
@@ -16,16 +16,10 @@ variable "lambda_role_name" {
   default = "job-post-lambda-handler-role"
 }
 
-variable "lambda_iam_policy_name" {
-  default = "job-post-lambda-handler-policy"
-}
 variable "lambda_layer" {
   default = "arn:aws:lambda:ap-southeast-1:940443069190:layer:job-post:1"
 }
-variable "zip_file" {
-  default = "job-post.zip"
-}
 variable "schedule" {
-  # The default value represents a cron expression scheduling a task to run every day at 12:00 AM. https://crontab.cronhub.io
+  # The default value represents a cron expression scheduling a task to run every day at 12:00 AM (UTC). https://crontab.cronhub.io
   default = "cron(0 0 * * ? *)"
 }
